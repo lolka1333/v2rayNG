c526b50a079749ec8831e26162247b166801cb17
lolka1333 <lolka1333@users.noreply.github.com>
2026-01-29 15:18:44 +0300

    up 2.0.7

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/BaseAdapterListener.kt
+++
@@ -1,8 +0,0 @@
-package com.v2ray.ang.ui
-
-interface BaseAdapterListener {
-    fun onEdit(guid: String, position: Int)
-    fun onRemove(guid: String, position: Int)
-    fun onShare(url: String)
-    fun onRefreshData()
-}
